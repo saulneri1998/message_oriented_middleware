6ba0e55a2870c21eaf1a72a7760610e49038d1c5
Saul Neri <saul.nerio.1998@gmail.com>
2020-05-07 22:44:49 -0500

    proxy implemented

---
+++ MediatorProxy.py
@@ -0,0 +1,22 @@
+from Mediator import Mediator
+from SimpleMediator import SimpleMediator
+
+class MediatorProxy(Mediator):
+
+    def __int__(self, __allowed_ids):
+        super().__init__()
+        self.allowed_ids = __allowed_ids
+        self.mediator = SimpleMediator()
+
+    def add_member(self, member):
+        if member.my_id in self.allowed_ids:
+            self.mediator.add_member(member)
+        else:
+            raise Exception("Authentication failed!!!")
+
+    def remove_member(self, member):
+        self.mediator.remove_member(member)
+
+    def notify(self, msg):
+        self.mediator.notify(msg)
+
--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 from SimpleMediator import SimpleMediator
+from MediatorProxy import MediatorProxy
 from Component import Component
 from Message import Message
 
 my_mediator = SimpleMediator()
-c1 = Component(my_mediator, "XML")
-c2 = Component(my_mediator, "JSON")
+my_mediator_proxy = MediatorProxy([1,3])
+
+try:   
+    c1 = Component(my_mediator, "XML")
+    c2 = Component(my_mediator, "JSON")
+except:
+    # nada
 
 c1.send_message(c2.my_id, "CSV", "\{\}")
 
